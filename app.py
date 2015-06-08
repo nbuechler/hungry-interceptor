27cdc04a8629a1bdcff2f064a248e1cc89a18aea
nbuechler <natebuechler@gmail.com>
2015-06-08 01:57:20 -0400

    Added logic to populate templates from database mongo2

--- app.py
+++ app.py
@@ -1,14 +1,47 @@
 from flask import Flask
+from flask import render_template
 from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
-mongo = PyMongo(app)
+# connect to MongoDB with the defaults
+mongo1 = PyMongo(app)
+
+## connect to another MongoDB database on the same host
+app.config['MONGO2_DBNAME'] = 'evgroio-dev'
+mongo2 = PyMongo(app, config_prefix='MONGO2')
+
+## connect to another MongoDB server altogether
+#app.config['MONGO3_HOST'] = 'another.host.example.com'
+#app.config['MONGO3_PORT'] = 27017
+#app.config['MONGO3_DBNAME'] = 'dbname_three'
+#mongo3 = PyMongo(app, config_prefix='MONGO3')
 
 @app.route('/')
 def home_page():
-    foo = mongo.db
+    foo = mongo2.db.users
     print(foo)
+    print(__name__)
+    print(2+2)
     return 'hello world'
+
+@app.route('/secret')
+def secret_page():
+    foo = mongo2.db.users
+    return 'shhh..this is a secret'
+
+@app.route('/users')
+@app.route('/users/<database>')
+def users(database=None):
+    all_users = mongo2.db.users.find({})
+    return render_template('users.html',
+        all_users=all_users, databse=database)
+
+@app.route('/logs')
+@app.route('/logs/<database>')
+def logs(database=None):
+    all_logs = mongo2.db.users.find({})
+    return render_template('logs.html',
+        all_logs=all_logs, databse=database)
 
 if __name__ == '__main__':
     app.run()
