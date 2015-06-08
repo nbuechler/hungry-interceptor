04f7a13b6f94ce24d6f758c657223cd781e49dde
nbuechler <natebuechler@gmail.com>
2015-06-08 02:13:28 -0400

    added logic to view logs

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ mongo1 = PyMongo(app)
 app.config['MONGO2_DBNAME'] = 'evgroio-dev'
 mongo2 = PyMongo(app, config_prefix='MONGO2')
 
+#'mongodb://evgroio01:admin@ds041238.mongolab.com:41238/heroku_app36697506'
+
 ## connect to another MongoDB server altogether
 #app.config['MONGO3_HOST'] = 'another.host.example.com'
 #app.config['MONGO3_PORT'] = 27017
@@ -34,14 +36,14 @@ def secret_page():
 def users(database=None):
     all_users = mongo2.db.users.find({})
     return render_template('users.html',
-        all_users=all_users, databse=database)
+        all_users=all_users, database=database)
 
 @app.route('/logs')
 @app.route('/logs/<database>')
 def logs(database=None):
-    all_logs = mongo2.db.users.find({})
+    all_logs = mongo2.db.logs.find({})
     return render_template('logs.html',
-        all_logs=all_logs, databse=database)
+        all_logs=all_logs, database=database)
 
 if __name__ == '__main__':
     app.run()
