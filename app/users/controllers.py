2ca9ce83fe14975dcdd1d709f5362ee664832d4f
nbuechler <natebuechler@gmail.com>
2015-12-04 20:17:24 -0500

    Added users controller

---
+++ app/users/controllers.py
@@ -0,0 +1,62 @@
+from flask import Blueprint
+from flask import render_template, redirect, url_for, jsonify
+
+#databases
+from config.database import mongo1, mongo2, mongo3, remoteDB1
+
+# mongo dependecies
+from flask.ext.pymongo import ObjectId
+
+# bson
+import json
+from bson import json_util
+
+users = Blueprint('users', __name__)
+
+@users.route('/')
+def tester():
+    print(2+2)
+    foo = mongo3.db.users
+    print(foo)
+    return 'Users!'
+
+@users.route('/<database>/users')
+def get_users(database=None):
+    if database == 'remote':
+        print('Receiving remote data')
+        all_users = remoteDB1.users.find({})
+    else:
+        database = 'default'
+        all_users = mongo3.db.users.find({})
+    return render_template('users.html',
+        all_users=all_users, database=database)
+
+#Go to new user create page
+@users.route('/<database>/users/new')
+def new_user(database=None):
+#    form = DefaultForm()
+    if database == 'remote':
+        print('Receiving remote data')
+    else:
+        database = 'default'
+    return render_template('newUser.html',
+#                           form=form,
+                           database=database
+                          )
+
+#Find a user by first name for fun!
+@users.route('/<database>/users/<first_name>')
+def get_user(database=None, first_name=None):
+    print('User is ' + first_name)
+    if database == 'remote':
+        print('Receiving remote data')
+        user = remoteDB1.users.find_one({'firstName': first_name})
+        print('==User==')
+        print(user)
+    else:
+        database = 'default'
+        user = mongo3.db.users.find_one({'firstName': first_name})
+        print('==User==')
+        print(user)
+    return render_template('userPage.html',
+        user=user, database=database)
