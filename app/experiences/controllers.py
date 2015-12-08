f363826431c6a8f9e0731f8c960af022255439f7
nbuechler <natebuechler@gmail.com>
2015-12-07 23:35:30 -0500

    fixed response generation

--- app/experiences/controllers.py
+++ app/experiences/controllers.py
@@ -47,14 +47,18 @@ def get_experiences(database=None):
 def process_experiences_overview(user=None):
         cursor = mongo3.db.experiences.find({"user": ObjectId('562d722a3f1f9f541814a3e8')}) #works! React User id
 
+        # Create a data dictionary to set up the building of data intended for different charts.
+        data_dict = {'data': []}
+
         main_return_dict = {'all' : []}
         for item in cursor:
             json_item = json.dumps(item, default=json_util.default)
 
             # Create a new python dictionary from the json_item, we'll call it json_dict
             json_dict = json.loads(json_item)
-
-        main_return_dict['all'].append(json_dict) # last json dict, and needs refactoring
+            data_dict['data'].append(json_dict)
+            
+        main_return_dict['all'].append(data_dict) # last json dict, and needs refactoring
         main_return_dict['all'].append({'another_data': 'No Data'})
         main_return_dict['all'].append({'description_primary': 'The experience information for every log you have written.'})
         main_return_dict['all'].append({'description_secondary': 'Use it wisely!'})
