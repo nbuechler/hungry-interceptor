8e54b0ee921209fbd01eccd30728d3380dd06f4a
nbuechler <natebuechler@gmail.com>
2015-12-23 23:42:34 -0500

    added some experience relationship wors and stuff

--- app/intercepts/controllers.py
+++ app/intercepts/controllers.py
@@ -31,10 +31,14 @@ This method also adds all the activites with the name attr because of course let
 '''
 @intercepts.route('/mongo2neo/intercepts_create_users')
 def intercepts_create_users():
-    user_cursor = mongo3.db.users.find({}) #find all users
 
+    # Clear the database
     secure_graph1.delete_all()
 
+    user_cursor = mongo3.db.users.find({}) #find all users
+    ####
+    # For every user create a node
+    ####
     for user in user_cursor:
         json_user = json.dumps(user, default=json_util.default)
 
@@ -45,24 +49,19 @@ def intercepts_create_users():
         new_user_node = Node("User", email=json_dict.get('email'), user_id=json_dict.get('_id').get('$oid'))
         secure_graph1.create(new_user_node)
 
-
-        # print '=====New====='
-        # print json_dict.get('email')
-        # print '============='
+        ####
         user = json_dict.get('_id').get('$oid')
-
-        activity_cursor = mongo3.db.activities.find({"user": ObjectId(user)}) #find all users
-
+        activity_cursor = mongo3.db.activities.find({"user": ObjectId(user)}) #find all activitiyes for a user
+        ####
+        # For every activity create a node
+        ####
         for activity in activity_cursor:
             json_activity = json.dumps(activity, default=json_util.default)
 
             # Create a new python dictionary from the json_activity, we'll call it json_dict
             json_dict = json.loads(json_activity)
 
-            # print json_dict.get('name')
-            print json_activity
-
-            # Create a bunch of user nodes
+            # Create a bunch of activity nodes
             new_activity_node = Node("Activity",
                 name=json_dict.get('name'),
                 activity_id=json_dict.get('_id').get('$oid'),
@@ -75,5 +74,36 @@ def intercepts_create_users():
                 secure_graph1.create(activity_has_word)
             user_did_activity = Relationship(new_user_node, "DID", new_activity_node)
             secure_graph1.create(user_did_activity)
+
+            ####
+            activity = json_dict.get('_id').get('$oid')
+            experience_cursor = mongo3.db.experiences.find({"firstActivity": ObjectId(activity)}) #find all activitiyes for a user
+            ####
+            # For every activity create a node
+            ####
+            for experience in experience_cursor:
+                json_experience = json.dumps(experience, default=json_util.default)
+
+                # Create a new python dictionary from the json_experience, we'll call it json_dict
+                json_dict = json.loads(json_experience)
+
+                # Create a bunch of experience nodes
+                new_experience_node = Node("Experience",
+                    name=json_dict.get('name'),
+                    experience_id=json_dict.get('_id').get('$oid'),
+                    privacy=json_dict.get('privacy'),
+                    word_length=json_dict.get('descriptionArrayLength'),
+                    )
+                for word in json_dict.get('descriptionArray'):
+                    new_word_node = Node("Word", name=word, characters=len(word))
+                    experience_has_word = Relationship(new_experience_node, "HAS", new_word_node)
+                    secure_graph1.create(experience_has_word)
+
+                activity_contains_experience = Relationship(new_activity_node, "CONTAINS", new_experience_node)
+                secure_graph1.create(activity_contains_experience)
+                user_experienced_experience = Relationship(new_user_node, "EXPERIENCED", new_experience_node)
+                secure_graph1.create(user_experienced_experience)
+
+
 
     return 'success'
