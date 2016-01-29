9312eced2ed0f51d0b3cd2239bfe9c1d17bb6a8d
nbuechler <natebuechler@gmail.com>
2016-01-28 23:29:06 -0500

    adding user unique word

--- app/users/controllers.py
+++ app/users/controllers.py
@@ -63,3 +63,58 @@ def get_user(database=None, first_name=None):
         print(user)
     return render_template('userPage.html',
         user=user, database=database)
+
+
+'''
+User SPOKE unique word, count of word usage
+MATCH ()-[r:SPOKE]->(n:Word) RETURN DISTINCT n.name, count(n.name)
+'''
+
+@users.route('/spoke/unique_word/<user>')
+def query_users_contains_unique_word(user=None):
+        cypher = secure_graph1.cypher
+        # Create a dictionary to hold the main object
+        main_return_dict = {'all' : []}
+
+        # Create a data dictionary to set up the building of data intended for different charts.
+        data_dict = {'data': []}
+
+        # Create a data dictionary to set up the building of data intended for different charts.
+        agr_data_dict = {'aggregateData': []}
+
+        # Create a dictionary to hold all the nodes
+        all_nodes_dict = {'allNodes': []}
+
+        # Create a dictionary to hold all the nodes
+        # source - the source node (an element in all_nodes_dict).
+        # target - the target node (an element in all_nodes_dict).
+        all_links_dict = {'allLinks': []}
+
+        # Create a dictionary to hold all the nodes
+        experience_nodes_dict = {'experienceNodes': []}
+
+        # Create a dictionary to hold all the nodes
+        word_nodes_dict = {'wordNodes': []}
+
+        for record in cypher.execute("MATCH (u:User {user_id: '" + user + "'})-[r:EXPERIENCED]->(experience)-[h:HAS]->(word) RETURN experience,word"):
+            print record
+
+        main_return_dict['all'].append(data_dict)
+        main_return_dict['all'].append(agr_data_dict)
+        main_return_dict['all'].append({'description_primary': 'This information is to show the clusters of words and their relationship to the experiences'})
+        main_return_dict['all'].append({'description_secondary': 'Use it wisely!'})
+        main_return_dict['all'].append({'title': 'Experience Clusters'})
+        main_return_dict['all'].append(all_links_dict)
+        main_return_dict['all'].append(all_nodes_dict)
+        main_return_dict['all'].append(experience_nodes_dict)
+        main_return_dict['all'].append(word_nodes_dict)
+        main_return_dict['all'].append({'totalLinks': len(all_links_dict['allLinks'])})
+        main_return_dict['all'].append({'totalNodes': len(all_nodes_dict['allNodes'])})
+        main_return_dict['all'].append({'totalExperiences': len(experience_nodes_dict['experienceNodes'])})
+        main_return_dict['all'].append({'totalWords': len(word_nodes_dict['wordNodes'])})
+
+        return jsonify(**main_return_dict)
+
+
+# Logro perspective? time filtered
+# TODO : MATCH ()-[r:DID]->(a)-[b:CONTAINS]->(e)-[c:CONTAINS]->(l) RETURN a, e, l
