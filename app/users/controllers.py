72c56366a406cc1e4c948056c4042ea214ab659c
nbuechler <natebuechler@gmail.com>
2016-01-28 23:40:04 -0500

    added actual wordcounts to python

--- app/users/controllers.py
+++ app/users/controllers.py
@@ -82,13 +82,8 @@ def query_users_contains_unique_word(user=None):
         # Create a data dictionary to set up the building of data intended for different charts.
         agr_data_dict = {'aggregateData': []}
 
-        # Create a dictionary to hold all the nodes
-        all_nodes_dict = {'allNodes': []}
 
-        # Create a dictionary to hold all the nodes
-        # source - the source node (an element in all_nodes_dict).
-        # target - the target node (an element in all_nodes_dict).
-        all_links_dict = {'allLinks': []}
+        all_unique_words_dict = {'allUniqueWords': []}
 
         # Create a dictionary to hold all the nodes
         experience_nodes_dict = {'experienceNodes': []}
@@ -98,20 +93,19 @@ def query_users_contains_unique_word(user=None):
 
         for record in cypher.execute("MATCH (u:User {user_id: '" + user + "'})-[r:SPOKE]->(word) RETURN DISTINCT word.name, count(word.name)"):
             print record
+            all_unique_words_dict['allUniqueWords'].append(
+                {
+                'word': record[0],
+                'count': record[1]
+                }
+            )
 
         main_return_dict['all'].append(data_dict)
         main_return_dict['all'].append(agr_data_dict)
-        main_return_dict['all'].append({'description_primary': 'This information is to show the clusters of words and their relationship to the experiences'})
+        main_return_dict['all'].append({'description_primary': 'The list of unique words and the number of times the words are used (frequency)'})
         main_return_dict['all'].append({'description_secondary': 'Use it wisely!'})
-        main_return_dict['all'].append({'title': 'Experience Clusters'})
-        main_return_dict['all'].append(all_links_dict)
-        main_return_dict['all'].append(all_nodes_dict)
-        main_return_dict['all'].append(experience_nodes_dict)
-        main_return_dict['all'].append(word_nodes_dict)
-        main_return_dict['all'].append({'totalLinks': len(all_links_dict['allLinks'])})
-        main_return_dict['all'].append({'totalNodes': len(all_nodes_dict['allNodes'])})
-        main_return_dict['all'].append({'totalExperiences': len(experience_nodes_dict['experienceNodes'])})
-        main_return_dict['all'].append({'totalWords': len(word_nodes_dict['wordNodes'])})
+        main_return_dict['all'].append({'title': 'Unique words'})
+        main_return_dict['all'].append(all_unique_words_dict)
 
         return jsonify(**main_return_dict)
 
