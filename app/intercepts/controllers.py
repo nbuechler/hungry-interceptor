eb14ef608364d18304319310d5d54e633dfcc72d
nbuechler <natebuechler@gmail.com>
2016-03-07 23:40:14 -0500

    added logic to create nodes

--- app/intercepts/controllers.py
+++ app/intercepts/controllers.py
@@ -75,30 +75,47 @@ def intercepts_create_single_activity(activity=None):
     print user_dict
 
     print 'here'
-    # for activity in activity_cursor:
-        # print activity
-        # json_activity = json.dumps(activity, default=json_util.default)
-        # # Create a new python dictionary from the json_activity, we'll call it json_dict
-        # json_dict = json.loads(json_activity)
-        #
-        # # Create a bunch of activity nodes
-        # new_activity_node = Node("Activity",
-        #     name=json_dict.get('name'),
-        #     activity_id=json_dict.get('_id').get('$oid'),
-        #     privacy=json_dict.get('privacy'),
-        #     word_length=json_dict.get('descriptionArrayLength'),
-        #     nodeType='activity',
-        #     )
-        #
-        # for word in json_dict.get('descriptionArray'):
-        #     new_word_node = Node("Word", name=word, characters=len(word), nodeType='word',)
-        #     activity_has_word = Relationship(new_activity_node, "HAS", new_word_node)
-        #     secure_graph1.create(activity_has_word)
-        #     user_spoke_word = Relationship(new_user_node, "SPOKE", new_word_node)
-        #     secure_graph1.create(user_spoke_word)
-        #
-        # user_did_activity = Relationship(new_user_node, "DID", new_activity_node)
-        # secure_graph1.create(user_did_activity)
+
+    ###
+    # TODO: Create an external method called create activity node.
+    # Business logic for ACTIVITIY_NODE starts here, uses data from above.
+    ###
+
+    # Create a new python dictionary from the json_user, we'll call it json_dict
+    json_dict = user_dict
+
+    # Create a bunch of user nodes
+    new_user_node = Node("User",
+        email=json_dict.get('email'),
+        user_id=json_dict.get('_id').get('$oid'),
+        nodeType='user',
+        )
+
+    ###
+    # TODO: Create an external method called create activity node.
+    # Business logic for ACTIVITIY_NODE starts here, uses data from above.
+    ###
+
+    json_dict = activity_dict
+
+    # Create a bunch of activity nodes
+    new_activity_node = Node("Activity",
+        name=json_dict.get('name'),
+        activity_id=json_dict.get('_id').get('$oid'),
+        privacy=json_dict.get('privacy'),
+        word_length=json_dict.get('descriptionArrayLength'),
+        nodeType='activity',
+        )
+    
+    for word in json_dict.get('descriptionArray'):
+        new_word_node = Node("Word", name=word, characters=len(word), nodeType='word',)
+        activity_has_word = Relationship(new_activity_node, "HAS", new_word_node)
+        secure_graph1.create(activity_has_word)
+        user_spoke_word = Relationship(new_user_node, "SPOKE", new_word_node)
+        secure_graph1.create(user_spoke_word)
+
+    user_did_activity = Relationship(new_user_node, "DID", new_activity_node)
+    secure_graph1.create(user_did_activity)
 
     return 'success'
 
