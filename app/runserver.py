9a5711d8df3b77444dc839419ca5008ae37c7012
nbuechler <natebuechler@gmail.com>
2017-01-01 22:43:08 -0500

    added method stub

--- app/runserver.py
+++ app/runserver.py
@@ -6,6 +6,7 @@ from activities.controllers import activities
 from friends.controllers import friends
 from users.controllers import users
 from intercepts.controllers import intercepts
+from nlp.controllers import nlp
 
 app.register_blueprint(logs, url_prefix='/logs')
 app.register_blueprint(experiences, url_prefix='/experiences')
@@ -13,6 +14,7 @@ app.register_blueprint(activities, url_prefix='/activities')
 app.register_blueprint(friends, url_prefix='/friends')
 app.register_blueprint(users, url_prefix='/users')
 app.register_blueprint(intercepts, url_prefix='/intercepts')
+app.register_blueprint(nlp, url_prefix='/nlp')
 
 # Sets the port, or defaults to 80
 if (len(sys.argv) > 1):
