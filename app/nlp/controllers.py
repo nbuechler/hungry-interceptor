b4833b178e4688dd20292373c102f82e5e0bc9db
nbuechler <natebuechler@gmail.com>
2017-02-10 22:22:16 -0500

    feat: Begin integrating ample-to-scorer path

--- app/nlp/controllers.py
+++ app/nlp/controllers.py
@@ -1,6 +1,10 @@
 from flask import Blueprint
 from flask import render_template, redirect, url_for, jsonify
 
+import requests, operator, math, json, csv, os
+
+from datetime import datetime
+
 #databases
 from config.databases import mongo1, mongo2, mongo3, remoteDB1, secure_graph1
 
@@ -10,6 +14,11 @@ from flask.ext.pymongo import ObjectId
 # bson
 import json
 from bson import json_util
+
+utc = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+utc_datetime = datetime.utcnow()
+api_ip = '0.0.0.0'
+port = '5000'
 
 nlp = Blueprint('nlp', __name__)
 
@@ -21,3 +30,21 @@ def tester():
 def save_record(database=None):
     # TODO: Add logic to save a record
     return "Not Implemented"
+
+@nlp.route('/analyze_emotion_set/big_6/', methods=['POST'])
+def analyze_emotion_set(database=None):
+
+    request_body = {
+      "doc": " Sadness is an emotional pain associated with, or characterized by, feelings of disadvantage, loss, despair, grief, helplessness, disappointment and sorrow. An individual experiencing sadness may become quiet or lethargic, and withdraw themselves from others. An example of severe sadness is depression. Crying is often an indication of sadness",
+      "lang": "english",
+      "natural": "1",
+      "stemmer": "1",
+      "lemma": "1",
+      "ub": 2,
+      "lb": 2
+    }
+
+    data = request_body
+    endpoint = 'http://' + api_ip + ':' + port + '/helpers/analyze_emotion_set/all_emotions/'
+    r = requests.post(endpoint, json=data)
+    return json.loads(r.content)
